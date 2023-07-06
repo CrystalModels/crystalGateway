f2e8cb0050244fb3392a8c0ff42dc475478c2d9f
Juan <juan.paez@api-ux.com>
2023-07-06 13:45:01 -0500

    Nueva funcionalidad

--- apiCore/v1/index.php
+++ apiCore/v1/index.php
@@ -2358,7 +2358,8 @@ Flight::route('GET /getOneUserByAdmin/@adminId/@profileId', function ($adminId,$
 
 Flight::route('GET /getProfileInfoLog/@userName', function ($userName) {
     
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: https://dev-app.crystalmodels.online");
+
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
