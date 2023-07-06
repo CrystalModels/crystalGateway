59bad5284a8489831132aac54533547c70df7daf
Juan <juan.paez@api-ux.com>
2023-07-06 16:42:38 -0500

    Nueva funcionalidad

--- apiIntegrations/v1/index.php
+++ apiIntegrations/v1/index.php
@@ -1401,7 +1401,7 @@ Flight::route('POST /postRooms/@headerslink', function ($headerslink) {
       
         $data = array(
           'ApiKey' =>$apiKey, 
-          'xapiKey' => $xApiKey
+          'xApiKey' => $xApiKey
           
           );
       $curl = curl_init();
