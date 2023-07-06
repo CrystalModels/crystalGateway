4529398f48f9bcd9d8997557fbbf30e5cbc51aaf
Juan <juan.paez@api-ux.com>
2023-07-06 17:26:25 -0500

    Nueva funcionalidad

--- apiIntegrations/v1/index.php
+++ apiIntegrations/v1/index.php
@@ -1429,7 +1429,7 @@ $dt=json_encode($dta);
       // Configurar las opciones de la sesión cURL
       curl_setopt($curl, CURLOPT_URL, $url);
       curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dta);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
       
       
