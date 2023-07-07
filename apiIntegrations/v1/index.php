d2227eb6b8acfcdfe083823dbe8fce9988da2f68
Juan <juan.paez@api-ux.com>
2023-07-06 19:01:39 -0500

    Nueva funcionalidad

--- apiIntegrations/v1/index.php
+++ apiIntegrations/v1/index.php
@@ -1423,8 +1423,8 @@ $dt=json_encode($dta);
       $sub_domaincon=new model_dom();
       $sub_domain=$sub_domaincon->domIntegrations();
 $headerslink1=$response1." ".$xApiKey;
-      $url1 = $sub_domain.'/crystalIntegrations/apiControlTower/v1/postRooms/'.$headerslink1;
-
+     // $url1 = $sub_domain.'/crystalIntegrations/apiControlTower/v1/postRooms/'.$headerslink1;
+      $url1 = $sub_domain.'/crystalIntegrations/apiControlTower/v1/postRooms1/';
       $curl1 = curl_init();
       
       curl_setopt($curl1, CURLOPT_URL, $url1);
