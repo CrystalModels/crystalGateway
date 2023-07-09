fb64fae1a3d79d1f646d677e0add8f0c1b6f5b43
Juan <juan.paez@api-ux.com>
2023-07-09 16:54:59 -0500

    Nueva funcionalidad}

--- apiCore/v1/index.php
+++ apiCore/v1/index.php
@@ -2578,4 +2578,83 @@ Flight::route('GET /getAllUsersGeneral/@headerslink/@profileId', function ($head
 
 });
 
+
+Flight::route('GET /getSessions/@headerslink/@userName', function ($headerslink,$userName) {
+    
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+    $parts = explode(" ", $headerslink);
+
+    $apiKey=$parts[0];
+    $xApiKey=$parts[1];
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apiKey) && !empty($xApiKey)) {
+        // Leer los datos de la solicitud
+       
+        // Acceder a los encabezados
+       
+        $sub_domaincon=new model_dom();
+        $sub_domain=$sub_domaincon->dom();
+        $url = $sub_domain.'/crystalCore/apiAuth/v1/authApiKeyLog/';
+      
+        $data = array(
+          
+          'xapiKey' => $xApiKey
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+        if ($response1 != 'false' ) {
+           
+
+
+            $sub_domaincons = new model_dom;
+            $sub_domain = $sub_domaincons->domIntegrations();
+            
+           
+            $response = file_get_contents($sub_domain.'/crystalCore/apiUsers/v1/getSessions/'.$xApiKey.'/'.$userName, false);
+      
+            // Realizar la solicitud y obtener la respuesta
+                 
+           
+        
+              echo $response;
+
+
+
+        } else {
+           echo 'Error: Autenticación fallida';
+             //echo json_encode($response1);
+           // echo $response1;
+        }
+    } else {
+        echo 'Error: Encabezados faltantes';
+    }
+
+
+
+
+
+
+});
+
+
+
 Flight::start();
