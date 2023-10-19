5f391c1e7c4c35de5e5364f7c774fdc89a997085
Juan <juan.paez@api-ux.com>
2023-10-19 17:26:33 -0500

    pr base de prueba

--- apiIntegrations/v1/index.php
+++ apiIntegrations/v1/index.php
@@ -2253,7 +2253,7 @@ $dt=json_encode($dta);
         // Realizar acciones basadas en los valores de los encabezados
   //echo "true";
 
-echo $response2." ".Flight::request()->data->pageId;
+echo $response2." acá".Flight::request()->data->pageId;
         
     } else {
         echo 'Error: Encabezados faltantes';
